0d4d483559227da1892cfc181568476b9a20c330
fernando <fernandoli1024@gmail.com>
2023-11-02 22:24:18 +0800

    本地发布完成

--- reeman-log/src/main/java/com/reeman/log/FileLoggingTree.java
+++ reeman-log/src/main/java/com/reeman/log/FileLoggingTree.java
@@ -1,7 +1,5 @@
 package com.reeman.log;
 
-import android.os.Environment;
-
 import com.elvishew.xlog.XLog;
 import com.elvishew.xlog.flattener.ClassicFlattener;
 import com.elvishew.xlog.printer.AndroidPrinter;
@@ -22,18 +20,19 @@ import timber.log.Timber;
 public class FileLoggingTree extends Timber.Tree {
 
     private final String defaultTAG;
+
     private final int logLevel;
     private final Map<String, Printer> printerMap = new HashMap<>();
 
     private final Printer androidPrinter = new AndroidPrinter(true);
 
 
-    public FileLoggingTree(int priority, String tag, List<String> pathList) {
+    public FileLoggingTree(int priority,String rootPath, String tag, List<String> pathList) {
         this.logLevel = priority;
         this.defaultTAG = tag;
         for (String path : pathList) {
             printerMap.put(path, new FilePrinter
-                    .Builder(Environment.getExternalStorageDirectory() + File.separator + path)
+                    .Builder(rootPath + File.separator + path)
                     .fileNameGenerator(new LogFileName(true))
                     .backupStrategy(new NeverBackupStrategy())
                     .flattener(new ClassicFlattener())
