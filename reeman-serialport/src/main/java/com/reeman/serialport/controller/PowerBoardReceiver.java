8de82ce9dd1ddcb8cbbe8220514e83cc77ec1aa5
lx <fernandoli1024@gmail.com>
2024-04-01 16:25:52 +0800

    电源板日志使用Timer

--- reeman-serialport/src/main/java/com/reeman/serialport/controller/PowerBoardReceiver.java
+++ reeman-serialport/src/main/java/com/reeman/serialport/controller/PowerBoardReceiver.java
@@ -1,14 +1,12 @@
 package com.reeman.serialport.controller;
 
-import android.os.Environment;
 
 import com.reeman.serialport.BuildConfig;
-import com.reeman.serialport.util.TimeUtil;
+import com.reeman.serialport.util.Parser;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Date;
+
+import timber.log.Timber;
 
 public class PowerBoardReceiver {
     private static PowerBoardReceiver INSTANCE;
@@ -19,22 +17,6 @@ public class PowerBoardReceiver {
             INSTANCE = new PowerBoardReceiver();
         }
         return INSTANCE;
-    }
-
-    public PowerBoardReceiver() {
-        File file = new File(Environment.getExternalStorageDirectory() + File.separator+BuildConfig.LOG_POWER_BOARD);
-        if (!file.exists() || !file.isDirectory()) {
-            file.mkdir();
-        } else {
-            File[] files = file.listFiles();
-            if (files != null && files.length > 0) {
-                for (int i = 0; i < files.length; i++) {
-                    if (shouldClean(files[i])) {
-                        files[i].delete();
-                    }
-                }
-            }
-        }
     }
 
     public void start(String port) throws Exception {
@@ -51,31 +33,6 @@ public class PowerBoardReceiver {
     }
 
     private void writeToLocal(byte[] data, int len) {
-        FileOutputStream downloadFile = null;
-        try {
-            downloadFile = new FileOutputStream(new File(Environment.getExternalStorageDirectory() + File.separator+BuildConfig.LOG_POWER_BOARD+File.separator + TimeUtil.formatDay(new Date()) + ".log"), true);
-            downloadFile.write(data, 0, len);
-            downloadFile.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (downloadFile != null)
-                    downloadFile.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-//            String strRead = new String(data, StandardCharsets.UTF_8);
-//            strRead = String.copyValueOf(strRead.toCharArray(), 0, len);
-//            Timber.tag(BuildConfig.LOG_ROS).e("power board : %s", strRead);
-
-        }
-
-    }
-
-    private boolean shouldClean(File file) {
-        long currentTimeMillis = System.currentTimeMillis();
-        long lastModified = file.lastModified();
-        return (currentTimeMillis - lastModified > 3 * 24 * 60 * 1000);
+        Timber.tag(BuildConfig.LOG_POWER_BOARD).w(Parser.bytesToASCII(data));
     }
 }
